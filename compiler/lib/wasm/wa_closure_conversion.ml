9a1c252962919d0d3367b1e5f2909ffed4ac2d53
Jérôme Vouillon <jerome.vouillon@gmail.com>
2023-02-13 23:09:07 +0100

    WIP

--- compiler/lib/wasm/wa_closure_conversion.ml
+++ compiler/lib/wasm/wa_closure_conversion.ml
@@ -2,4 +2,92 @@
 A lot like lambda lifting
 ===> get function free variables
 ===> merge closure of mutually recursive functions
+
+Free variables of a function:
+- immediate free variables
+- free variables from subfunctions which are not bound
 *)
+
+open! Stdlib
+open Code
+
+let collect_free_vars program var_depth depth pc =
+  let vars = ref Var.Set.empty in
+  let rec traverse pc =
+    Code.preorder_traverse
+      { fold = Code.fold_children }
+      (fun pc () ->
+        let block = Code.Addr.Map.find pc program.blocks in
+        Freevars.iter_block_free_vars
+          (fun x ->
+            let idx = Var.idx x in
+            if idx < Array.length var_depth
+            then (
+              let d = var_depth.(idx) in
+              assert (d >= 0);
+              if d < depth then vars := Var.Set.add x !vars))
+          block;
+        List.iter block.body ~f:(fun i ->
+            match i with
+            | Let (_, Closure (_, (pc', _))) -> traverse pc'
+            | _ -> ()))
+      pc
+      program.blocks
+      ()
+  in
+  traverse pc;
+  !vars
+
+let mark_bound_variables var_depth block depth =
+  Freevars.iter_block_bound_vars (fun x -> var_depth.(Var.idx x) <- depth) block;
+  List.iter block.body ~f:(fun i ->
+      match i with
+      | Let (_, Closure (params, _)) ->
+          List.iter params ~f:(fun x -> var_depth.(Var.idx x) <- depth + 1)
+      | _ -> ())
+
+let rec traverse var_depth (program, functions) pc depth =
+  Code.preorder_traverse
+    { fold = Code.fold_children }
+    (fun pc (program, functions) ->
+      let block = Code.Addr.Map.find pc program.blocks in
+      mark_bound_variables var_depth block depth;
+      let rec rewrite_body st l =
+        match l with
+        | Let (f, (Closure (_, (pc', _)) as cl)) :: rem ->
+            let program, functions = traverse var_depth st pc' (depth + 1) in
+            let free_vars = collect_free_vars program var_depth (depth + 1) pc' in
+            let s =
+              Var.Set.fold
+                (fun x m -> Var.Map.add x (Var.fork x) m)
+                free_vars
+                Var.Map.empty
+            in
+            let program = Subst.cont (Subst.from_map s) pc' program in
+            let f' = try Var.Map.find f s with Not_found -> Var.fork f in
+            let s = Var.Map.bindings (Var.Map.remove f s) in
+            let f'' = Var.fork f in
+            let pc'' = program.free_pc in
+            let bl = { params = []; body = [ Let (f', cl) ]; branch = Return f' } in
+            let program =
+              { program with
+                free_pc = pc'' + 1
+              ; blocks = Addr.Map.add pc'' bl program.blocks
+              }
+            in
+            let functions =
+              Let (f'', Closure (List.map s ~f:snd, (pc'', []))) :: functions
+            in
+            let rem', st = rewrite_body (program, functions) rem in
+            Let (f, Apply { f = f''; args = List.map ~f:fst s; exact = true }) :: rem', st
+        | i :: rem ->
+            let rem', st = rewrite_body st rem in
+            i :: rem', st
+        | [] -> [], st
+      in
+      let body, (program, functions) = rewrite_body (program, functions) block.body in
+      ( { program with blocks = Addr.Map.add pc { block with body } program.blocks }
+      , functions ))
+    pc
+    program.blocks
+    (program, functions)
--- compiler/lib/wasm/wa_generate.ml
+++ compiler/lib/wasm/wa_generate.ml
@@ -5,6 +5,12 @@ module I32 = Wasm.I32
 module Source = Wasm.Source
 open Code
 
+type ctx =
+  { live : int array
+  ; global : bool
+  ; blocks : block Addr.Map.t
+  }
+
 type var =
   | Global of Ast.var
   | Local of Ast.var
@@ -14,11 +20,6 @@ type state =
   { var_count : int
   ; vars : var Var.Map.t
   ; instrs : Ast.instr list
-  }
-
-type ctx =
-  { live : int array
-  ; global : bool
   }
 
 type 'a t = state -> 'a * state
@@ -32,8 +33,18 @@ let return x st = x, st
 
 let var x st = Var.Map.find x st.vars, st
 
+let add_var ~global x ({ var_count; vars; _ } as st) =
+  let i = Source.(Int32.of_int var_count @@ no_region) in
+  let vars = Var.Map.add x (if global then Global i else Local i) vars in
+  i, { st with var_count = var_count + 1; vars }
+
 let instr i : unit t =
  fun st -> (), { st with instrs = Source.(i @@ no_region) :: st.instrs }
+
+let blk l st =
+  let instrs = st.instrs in
+  let (), st = l { st with instrs = [] } in
+  List.rev st.instrs, { st with instrs }
 
 module Arith = struct
   let binary op e e' =
@@ -58,7 +69,7 @@ module Memory = struct
     let* () = e' in
     instr (Operators.i32_store 4 (Int32.of_int offset))
 
-  let allocate ~tag:_ ~args:_ = [] (*ZZZ*)
+  let allocate ~tag:_ _a _array_or_not = return () (*ZZZ Float array?*)
 
   let tag e = load ~offset:(-4) e
 
@@ -86,13 +97,27 @@ let assign x e =
   | Global x -> instr (Operators.global_set x)
   | Env _ -> assert false
 
+let drop e =
+  let* () = e in
+  instr Operators.drop
+
+let loop l =
+  let* instrs = blk l in
+  instr (Operators.loop (ValBlockType None) instrs)
+
+let block l =
+  let* instrs = blk l in
+  instr (Operators.block (ValBlockType None) instrs)
+
+let if_ l1 l2 =
+  let* instrs1 = blk l1 in
+  let* instrs2 = blk l2 in
+  instr (Operators.if_ (ValBlockType None) instrs1 instrs2)
+
 let store ctx x e =
-  ( let* ) e (fun () ({ var_count; vars; _ } as st) ->
-      let i = Source.(Int32.of_int var_count @@ no_region) in
-      let vars = Var.Map.add x (if ctx.global then Global i else Local i) vars in
-      instr
-        (if ctx.global then Operators.global_set i else Operators.local_set i)
-        { st with var_count = var_count + 1; vars })
+  let* () = e in
+  let* i = add_var ~global:ctx.global x in
+  instr (if ctx.global then Operators.global_set i else Operators.local_set i)
 
 let rec translate_expr e =
   match e with
@@ -106,7 +131,7 @@ let rec translate_expr e =
         let* () = return () (* load function *) in
         instr Operators.(call_indirect Source.(0l @@ no_region) Source.(0l @@ no_region))
       else return () (*ZZZ*)
-  | Block (_tag, _a, _array_or_not) -> return () (*ZZZ*)
+  | Block (tag, a, array_or_not) -> Memory.allocate ~tag a array_or_not
   | Field (x, n) -> Memory.field (load x) n
   | Closure (_args, ((_pc, _) as _cont)) -> return () (*ZZZ*)
   | Constant _c -> return () (*ZZZ*)
@@ -117,34 +142,68 @@ and translate_instr ctx i =
   | Assign (x, y) -> assign x (load y)
   | Let (x, e) ->
       if ctx.live.(Var.idx x) = 0
-      then
-        let* () = translate_expr e in
-        instr Operators.drop
+      then drop (translate_expr e)
       else store ctx x (translate_expr e)
   | Set_field (x, n, y) -> Memory.set_field (load x) n (load y)
   | Offset_ref (x, n) ->
       Memory.set_field (load x) 0 Arith.(Memory.field (load x) 0 + const (2 * n))
   | Array_set (x, y, z) -> Memory.array_set (load x) (load y) (load z)
 
-(*
-let compile_closure ctx (pc, args) =
-  let st = build_graph ctx pc in
-  let current_blocks = !(st.visited_blocks) in
-  st.visited_blocks := Addr.Set.empty;
-  if debug () then Format.eprintf "@[<hv 2>closure {@;";
-  let backs = Addr.Set.empty in
-  let loop_stack = [] in
-  incr_seen st pc;
-  let _never, res =
-    compile_branch st [] (pc, args) loop_stack backs Addr.Set.empty Interm.empty
+and translate_instrs ctx l =
+  match l with
+  | [] -> return ()
+  | i :: rem ->
+      let* () = translate_instr ctx i in
+      translate_instrs ctx rem
+
+let compile_closure ctx _name_opt _params (pc, _args) =
+  let g = Wa_structure.build_graph ctx.blocks pc in
+  let idom = Wa_structure.dominator_tree g in
+  let _dom = Wa_structure.reverse_tree idom in
+
+  let rec index pc i context =
+    match context with
+    | (`Loop pc' | `Block pc') :: _ when pc = pc' -> i
+    | _ :: rem -> index pc (i + 1) rem
+    | [] -> assert false
   in
-  if Addr.Set.cardinal !(st.visited_blocks) <> Addr.Set.cardinal current_blocks
-  then (
-    let missing = Addr.Set.diff current_blocks !(st.visited_blocks) in
-    Format.eprintf "Some blocks not compiled %s!@." (string_of_set missing);
-    assert false);
-  if debug () then Format.eprintf "}@]@;";
-  res
+  let open Source in
+  let rec do_tree pc context =
+    let code =
+      node_within
+        pc
+        (List.filter
+           (fun pc' -> Wa_structure.is_merge_node g pc')
+           (List.rev (Addr.Set.elements (Wa_structure.get_edges g.succs pc))))
+    in
+    if Wa_structure.is_loop_header g pc
+    then loop (code (`Loop pc :: context))
+    else code context
+  and node_within pc l context =
+    match l with
+    | pc' :: rem ->
+        let* () = block (node_within pc rem (`Block pc' :: context)) in
+        do_tree pc' context
+    | [] -> (
+        let block = Addr.Map.find pc ctx.blocks in
+        let* () = translate_instrs ctx block.body in
+        match block.branch with
+        | Branch (pc', _) -> (*ZZZ*) do_branch pc pc' context
+        | Return x ->
+            let* () = load x in
+            instr Operators.return
+        | Cond (x, (pc1, _), (pc2, _)) ->
+            let* () = load x in
+            if_ (do_branch pc pc1 (`If :: context)) (do_branch pc pc2 (`If :: context))
+        | Stop -> instr Operators.return
+        | Switch (_, _, _) -> assert false
+        | Raise _ | Pushtrap _ | Poptrap _ -> assert false (*ZZZ*))
+  and do_branch src dst context =
+    if Wa_structure.is_backward g src dst || Wa_structure.is_merge_node g dst
+    then instr (Operators.br (Int32.of_int (index dst 0 context) @@ no_region))
+    else do_tree dst context
+  in
+  ignore (do_tree pc [] { var_count = 0; vars = Var.Map.empty; instrs = [] })
 
 let compile_program ctx pc =
   let res = compile_closure ctx (pc, []) in
@@ -152,13 +211,15 @@ let compile_program ctx pc =
 
 let f
     (p : Code.program)
-    ~exported_runtime
     ~live_vars
+    (*
     ~cps_calls
     ~should_export
     ~warn_on_unhandled_effect
-    debug =
-  let ctx = { live = live_vars } in
-  let p = compile_program ctx p.start in
-  p
 *)
+      _debug =
+  let ctx = { live = live_vars; blocks = p.blocks; global = true } in
+  Code.fold_closures
+    p
+    (fun name_opt params cont () -> compile_closure ctx name_opt params cont)
+    ()
