dc89650e338c7f929497654daad20427e37abda0
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-03-27 22:22:20 +0100

    DEBUG

--- compiler/lib/wasm/wa_link.ml
+++ compiler/lib/wasm/wa_link.ml
@@ -629,7 +629,7 @@ let link ~output_file ~linkall ~files =
     if times () then Format.eprintf "  emit: %a@." Timer.print t;
     if n > 0 then loop (n - 1)
   in
-  loop 99
+  loop 0
 
 let link ~output_file ~linkall ~files =
   try link ~output_file ~linkall ~files
